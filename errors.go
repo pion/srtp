4ae126ef295462530ea7d5601ee3ac103a0d894a
Atsushi Watanabe <atsushi.w@ieee.org>
2020-07-28 16:16:28 +0900

    Add details of duplicated error
    It was not clear which stream caused error in the session.
    Add details of duplication error.

--- errors.go
+++ errors.go
@@ -2,6 +2,21 @@ package srtp
 
 import (
 	"errors"
+	"fmt"
 )
 
 var errDuplicated = errors.New("duplicated packet")
+
+type errorDuplicated struct {
+	Proto string // srtp or srtcp
+	SSRC  uint32
+	Index uint32 // sequence number or index
+}
+
+func (e *errorDuplicated) Error() string {
+	return fmt.Sprintf("%s ssrc=%d index=%d: %v", e.Proto, e.SSRC, e.Index, errDuplicated)
+}
+
+func (e *errorDuplicated) Unwrap() error {
+	return errDuplicated
+}
